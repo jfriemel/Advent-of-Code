29ccdae35bb820c6e60c67b7319f29ba77f63b2e
Jonas Friemel <jfriemel@techfak.uni-bielefeld.de>
2020-12-15 15:13:01 +0100

    AoC 2020 day 15.

---
+++ 2020/day15_p2.hs
@@ -0,0 +1,25 @@
+import qualified Data.IntMap as IM  (IntMap, fromList, insert, lookup)
+import           Data.List.Split    (splitOn)
+import           Data.Maybe         (fromMaybe)
+import           System.Environment (getArgs)
+
+{- Advent of Code 2020 - Day 15 - Puzzle 2 -}
+
+main :: IO ()
+main = do
+  args <- getArgs
+  run (head args)
+
+run :: String -> IO ()
+run file = do
+  input <- readFile file
+  let inputList = map read $ splitOn "," input
+      len = length inputList
+      n = 30000000
+  putStrLn $ show n ++ "th number: " ++ show (play n len (last inputList) (IM.fromList (zip (init inputList) [1..]))) ++ "."
+
+play :: Int -> Int -> Int -> IM.IntMap Int -> Int
+play n i l im 
+  | i == n    = l
+  | otherwise = play n (i+1) next $! IM.insert l i im
+  where next = i - fromMaybe i (IM.lookup l im)
